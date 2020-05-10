93d74483abaae5f1f9db7d8448a120eab7806369
thelxz <thelxz@163.com>
2020-05-10 14:02:58 +0800

    change debug port to SWD.

--- Core/Src/stm32f1xx_hal_msp.c
+++ Core/Src/stm32f1xx_hal_msp.c
@@ -72,9 +72,9 @@ void HAL_MspInit(void)
 
   /* System interrupt init*/
 
-  /** ENABLE: Full SWJ (JTAG-DP + SW-DP): Reset State 
+  /** NOJTAG: JTAG-DP Disabled and SW-DP Enabled 
   */
-  __HAL_AFIO_REMAP_SWJ_ENABLE();
+  __HAL_AFIO_REMAP_SWJ_NOJTAG();
 
   /* USER CODE BEGIN MspInit 1 */
 
